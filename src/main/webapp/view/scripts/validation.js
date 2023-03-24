d8d9abe0c44e3abf69da2077cc66b23e9c6d17d3
Salma-Abdallah <119084967+Salma-Abdallah@users.noreply.github.com>
2023-03-24 14:33:19 +0200

    Create validation.js

---
+++ src/main/webapp/view/scripts/validation.js
@@ -0,0 +1,44 @@
+function passwordValidate(){
+    let password=document.getElementById("password").value;
+    let regex =/(.+){8}/;
+
+    let result=regex.test(password);
+
+    if(result){
+        document.getElementById("msgPassword").innerHTML="Valid Password";
+        document.getElementById("msgPassword").style.color="green";
+    }
+    else{
+        document.getElementById("msgPassword").innerHTML="InValid Password";
+        document.getElementById("msgPassword").style.color="red";
+    }
+}
+
+function cofirmPasswordValidate(){
+    let password=document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+
+    if(password!=confirmPassword){
+        document.getElementById("msgConfirmPass").innerHTML="InCorrect Password";
+        document.getElementById("msgConfirmPass").style.color="red";
+    }
+        
+}
+
+function nameValidate(){
+    let name=document.getElementById("")
+    let regex=/[A-Za-z]+$/;
+
+    let result=regex.test(name);
+
+    return result;
+    
+}
+
+function phoneValidate(){
+    let phone = document.getElementById("");
+    let regex = /01(2|1|0|5)[0-9]{8}$/;
+    let result = regex.test(phone);
+
+    return result;
+}
