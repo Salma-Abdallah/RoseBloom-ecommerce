1248de39d943fe2b37373af20869962a33aef315
omniayman <58345026+omniayman@users.noreply.github.com>
2023-03-26 22:28:52 +0200

    Update ProductRepository.java

--- src/main/java/com/rosebloom/controllers/persistence/repository/ProductRepository.java
+++ src/main/java/com/rosebloom/controllers/persistence/repository/ProductRepository.java
@@ -1,11 +1,13 @@
 package com.rosebloom.controllers.persistence.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.rosebloom.controllers.persistence.connection.EntityManagerFactorySingleton;
 import com.rosebloom.controllers.persistence.entities.Category;
 import com.rosebloom.controllers.persistence.entities.Product;
 import com.rosebloom.controllers.persistence.entities.ProductImage;
+import com.rosebloom.controllers.persistence.entities.ProductImageId;
 import com.rosebloom.dtos.ProductImageDto;
 
 import jakarta.persistence.EntityManager;
@@ -44,7 +46,8 @@ public class ProductRepository  {
   
     public void editProduct(Product product) {
         Product product1=entityManager.find(Product.class,product.getId());  
-        
+        editProductImage(new ArrayList<>(product.getProductImages()),product.getId());
+
          entityManager.getTransaction().begin();
         // product1.setCategory(product.getCategory());
          product1.setName(product.getName());
@@ -52,15 +55,15 @@ public class ProductRepository  {
          product1.setOldPrice(product.getOldPrice());
          product1.setQuantity(product.getQuantity());
          product1.setDescription(product.getDescription());
-        product1.setProductImages(product.getProductImages());
-        for (ProductImage img : product.getProductImages()) {
-            System.out.println(img.getImage());
+        // product1.setProductImages(product.getProductImages());
+        // for (ProductImage img : product.getProductImages()) {
+        //     System.out.println(img.getImage());
 
-        }
-        for (ProductImage img : product1.getProductImages()) {
-            System.out.println(img.getImage());
+        // }
+        // for (ProductImage img : product1.getProductImages()) {
+        //     System.out.println(img.getImage());
             
-        }
+        // }
          product1.setCategories(product.getCategories());
          product1.setCreatedAt(product.getCreatedAt());
          product1.setSize(product.getSize());
@@ -71,6 +74,34 @@ public class ProductRepository  {
             System.out.println(img.getImage());
             
         }
+    }
+    private void editProductImage(List<ProductImage> productImages,int pId) {
+        Query query = entityManager.createQuery("from ProductImage p where p.product.id=?1").setParameter(1, pId);
+       
+        List<ProductImage> result = (List<ProductImage>) query.getResultList();
+        System.out.println(result.size());
+        System.out.println(productImages.size());
+        for (int i=0;i<result.size();i++) {
+            ProductImageId productImageId=new ProductImageId(result.get(i).getId().getImgId(), pId);
+            ProductImage productImg=entityManager.find(ProductImage.class,productImageId); 
+            entityManager.getTransaction().begin();
+            productImg.setImage(productImages.get(i).getImage());
+            entityManager.getTransaction().commit();
+           // System.out.println(productImage.getId().getImgId());
+        }
+        if(productImages.size()>result.size()) {
+            for(int i=result.size(); i<productImages.size();i++)
+            {
+                System.out.println(productImages.get(i));
+                ProductImageId productImageId=new ProductImageId();
+                productImageId.setProductId(pId);
+                productImages.get(i).setId(productImageId);
+                entityManager.getTransaction().begin();
+                entityManager.persist(productImages.get(i));
+                entityManager.getTransaction().commit();
+            }
+        }
+        
     }
 
     // @Override
